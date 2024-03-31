db921da3261bb192567a8f2f88276ee43ec6fe03
ishanghimire11 <ishanghimire11@gmail.com>
2024-03-31 12:45:14 +0545

    feat: added routes and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import About from "@/pages/About";
+import Dashboard from "@/pages/Dashboard";
+import Home from "@/pages/Home";
+import Projects from "@/pages/Projects";
+import Signin from "@/pages/Sign-in";
+import Signup from "@/pages/Sign-up";
 
 const App = () => {
-  return <div className="text-3xl">GG</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
