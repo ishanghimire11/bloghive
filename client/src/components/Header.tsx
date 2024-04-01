90ecd97df48735da46c33a3b934109f88cec67a5
ishanghimire11 <ishanghimire11@gmail.com>
2024-04-01 23:16:49 +0545

    feat: responsive header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,27 +1,86 @@
-import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import Navlinks from "@/components/Navlinks";
+import { HeaderSearch } from "@/types/types";
+import { NavLink } from "react-router-dom";
+import Logo from "@/assets/logo.svg";
+import { LogInIcon } from "lucide-react";
 
 const Header = () => {
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm<HeaderSearch>();
+  const onSubmit: SubmitHandler<HeaderSearch> = (data) => console.log(data);
+
   return (
-    <div className="flex items-center justify-between px-6 py-5 ">
-      logo
-      <Navlinks />
-      <div>
-        <label className="flex items-center gap-2 input input-bordered">
-          <input type="text" className="grow" placeholder="Search" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
+    <div className="px-6 py-5 border-b sm:flex sm:gap-4 sm:items-center sm:justify-between sm:flex-row border-b-base-300">
+      <div className={"inline-block w-1/2 sm:w-fit sm:block"}>
+        <NavLink to={"/"}>
+          <img src={Logo} />
+        </NavLink>
+      </div>
+
+      <div className="hidden lg:items-center lg:flex lg:-order-1">
+        <Navlinks />
+      </div>
+
+      <div
+        className={
+          "inline-block text-right w-1/2 -order-1 sm:w-fit sm:block lg:hidden"
+        }
+      >
+        <details className="block ham-menu dropdown lg:hidden">
+          <summary className="p-2 m-1 rounded-full btn">
+            <svg
+              className="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 512 512"
+            >
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
+          </summary>
+          <div className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <Navlinks />
+          </div>
+        </details>
+      </div>
+
+      <div className="flex w-full mt-4 gap-x-2 sm:w-fit sm:mt-0">
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full sm:w-fit">
+          <label className="flex items-center gap-2 input input-bordered">
+            <input
+              type="text"
+              className="grow"
+              placeholder="Search"
+              {...register("query", { required: true })}
             />
-          </svg>
-        </label>
+            <button type="submit">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </label>
+          {/* {errors.query && (
+            <span className="text-red-500">Add a search query</span>
+          )} */}
+        </form>
+        <button className="hidden btn btn-primary sm:flex sm:gap-x-2 ">
+          <LogInIcon />
+          <span>Login</span>
+        </button>
       </div>
     </div>
   );
--- client/src/components/Navlinks.tsx
+++ client/src/components/Navlinks.tsx
@@ -1,10 +1,14 @@
+import { LogInIcon } from "lucide-react";
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+const navLinkHover =
+  "lg:relative lg:w-fit lg:block lg:after:block lg:after:content-[''] lg:after:absolute lg:after:h-[2px] lg:after:bg-black lg:after:w-full lg:after:scale-x-0 lg:after:hover:scale-x-100 lg:after:transition lg:after:duration-300 lg:after:origin-left";
 
 const Navlinks = () => {
   return (
     <nav>
-      <ul className="flex items-center gap-x-6">
+      <ul className="flex flex-col lg:flex-row lg:items-center gap-x-8">
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
@@ -16,6 +20,15 @@ const Navlinks = () => {
         </li>
         <li>
           <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li className="block w-full pt-4 mt-2 border-t sm:hidden border-t-base-300">
+          <button
+            type="button"
+            className="flex justify-start w-full px-4 py-2 text-left h-fit btn btn-primary min-h-fit gap-x-2"
+          >
+            <LogInIcon />
+            <span>Login</span>
+          </button>
         </li>
       </ul>
     </nav>
