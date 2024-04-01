1aecfc74076b46a88f599a909cb87ea829876156
ishanghimire11 <ishanghimire11@gmail.com>
2024-04-02 01:41:14 +0545

    feat: added simple get routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -16,3 +18,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} !!`);
 });
+
+app.use("/api/user", userRoutes);
