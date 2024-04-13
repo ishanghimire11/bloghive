8267a5465b0e03d28c5e0b440eaa328399cceaff
ishanghimire11 <ishanghimire11@gmail.com>
2024-04-13 23:55:44 +0545

    feat: added private dahshboard route

--- client/src/components/Navlinks.tsx
+++ client/src/components/Navlinks.tsx
@@ -1,11 +1,15 @@
+import { RootState } from "@/redux/store";
 import { LogInIcon } from "lucide-react";
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const navLinkHover =
   "lg:relative lg:w-fit lg:block lg:after:block lg:after:content-[''] lg:after:absolute lg:after:h-[2px] lg:after:bg-black lg:after:w-full lg:after:scale-x-0 lg:after:hover:scale-x-100 lg:after:transition lg:after:duration-300 lg:after:origin-left";
 
 const Navlinks = () => {
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
   return (
     <nav>
       <ul className="flex flex-col lg:flex-row lg:items-center gap-x-8">
@@ -16,20 +20,22 @@ const Navlinks = () => {
           <NavLink to="/about">About</NavLink>
         </li>
         <li>
-          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/dashboard?tab=profile">Dashboard</NavLink>
         </li>
         <li>
           <NavLink to="/projects">Projects</NavLink>
         </li>
-        <li className="block w-full pt-4 mt-2 border-t sm:hidden border-t-base-300">
-          <button
-            type="button"
-            className="flex justify-start w-full px-4 py-2 text-left h-fit btn btn-primary min-h-fit gap-x-2"
-          >
-            <LogInIcon />
-            <span>Login</span>
-          </button>
-        </li>
+        {!currentUser && (
+          <li className="block w-full pt-4 mt-2 border-t sm:hidden border-t-base-300">
+            <NavLink
+              to={"/sign-in"}
+              className="flex justify-start w-full px-4 py-2 text-left h-fit btn btn-primary min-h-fit gap-x-2"
+            >
+              <LogInIcon />
+              <span>Login</span>
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { RootState } from "@/redux/store";
+
+const PrivateRoute = () => {
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to={"/sign-in"} />;
+};
+
+export default PrivateRoute;
