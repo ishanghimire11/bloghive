2a996b92d8c0129b9b66d7d9f3934fd56232f93b
ishanghimire11 <ishanghimire11@gmail.com>
2024-04-15 22:35:33 +0545

    feat: code cleanup and improving import order

--- client/src/components/Dashboard/Tabs.tsx
+++ client/src/components/Dashboard/Tabs.tsx
@@ -1,5 +1,5 @@
 import { DashboardTabsProps } from "@/types/types";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 export const Tabs = ({ currentTab, setCurrentTab }: DashboardTabsProps) => {
