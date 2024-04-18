885b843efa8d41e74a862549078783f67965b263
ishanghimire11 <ishanghimire11@gmail.com>
2024-04-18 23:22:48 +0545

    feat: added image upload to firebase

--- client/src/components/Dashboard/Profile.tsx
+++ client/src/components/Dashboard/Profile.tsx
@@ -2,8 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2Icon, PlusIcon } from "lucide-react";
+import { AlertCircleIcon, Loader2Icon, PlusIcon } from "lucide-react";
 import { RootState } from "@/redux/store";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 import { signupFields } from "@/constants/constants";
 import {
@@ -12,7 +14,6 @@ import {
   registerUserSchema,
 } from "@/validation/validation";
 import {
-  StorageReference,
   getDownloadURL,
   getStorage,
   ref,
@@ -22,8 +23,10 @@ import { app } from "@/firebase";
 
 export const Profile = () => {
   const [imageFiles, setImageFiles] = useState<File | null>(null);
-  const [imageUrl, setImageUrl] = useState<string | StorageReference>("");
-  const [imageUploadProgress, setImageUploadProgress] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [imageUploadProgress, setImageUploadProgress] = useState<number | null>(
+    null
+  );
   const [imageUploadError, setImageUploadError] = useState<string>("");
   const filePickRef = useRef<HTMLInputElement | null>(null);
 
@@ -42,35 +45,26 @@ export const Profile = () => {
     if (imageFiles) {
       const fileName = new Date().getTime() + imageFiles.name;
       const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, imageFiles);
       uploadTask.on(
         "state_changed",
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setImageUploadProgress(progress?.toFixed(0));
-          console.log("Upload is " + progress + "% done");
+          setImageUploadProgress(+progress?.toFixed(0));
         },
         (error) => {
-          setImageUploadError("Could not upload");
+          setImageUploadError("Upload Failed. Image must be less than 2 MB");
+          setImageFiles(null);
+          setImageUrl("");
         },
         async () => {
-          const getDownloadURL = await uploadTask.snapshot.ref;
-          setImageUrl(getDownloadURL);
+          const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+          setImageUrl(downloadUrl);
         }
       );
     }
   };
-
-  console.log(imageUrl, "imageUrlimageUrlimageUrlimageUrl");
-  console.log(
-    imageUploadProgress,
-    "imageUploadProgressimageUploadProgressimageUploadProgressimageUploadProgress"
-  );
-  console.log(
-    imageUploadError,
-    "imageUploadErroimageUploadErroimageUploadErro"
-  );
 
   useEffect(() => {
     if (imageFiles) {
@@ -89,9 +83,30 @@ export const Profile = () => {
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
-    if (files && files.length > 0) {
-      return setImageFiles(files[0]);
+
+    if (!files || files.length === 0) {
+      return;
     }
+
+    const selectedFile = files[0];
+    const { type, size } = selectedFile;
+
+    const maxSize = 2 * 1024 * 1024;
+    if (size > maxSize) {
+      setImageUploadError("Image must be less than 2 MB");
+      setImageUploadProgress(null);
+      return;
+    }
+
+    // Check file type
+    const [mainType, subType] = type.split("/");
+    if (mainType !== "image") {
+      setImageUploadError("Please upload an image file");
+      setImageUploadProgress(null);
+      return;
+    }
+    setImageUploadError("");
+    setImageFiles(selectedFile);
   };
 
   const onSubmit: SubmitHandler<RegisterUserSchema> = (data) =>
@@ -115,6 +130,22 @@ export const Profile = () => {
           className="relative w-32 h-32 p-2 mb-6 rounded-full cursor-pointer group bg-primary"
           onClick={() => filePickRef.current?.click()}
         >
+          <div className="absolute top-0 left-0 w-full h-full text-center">
+            {imageUploadProgress && (
+              <CircularProgressbar
+                value={imageUploadProgress}
+                styles={{
+                  root: {
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                  },
+                }}
+              />
+            )}
+          </div>
           <img
             src={imageUrl || (currentUser && currentUser.photoUrl) || ""}
             alt="user"
@@ -125,6 +156,13 @@ export const Profile = () => {
             <span className="text-xs opacity-70">Replace image</span>
           </div>
         </div>
+        {imageUploadError && (
+          <div className="flex items-center w-full p-4 pl-6 text-sm leading-none rounded-lg text-error bg-error/15 gap-x-2">
+            <AlertCircleIcon className="w-5 h-5" />
+            <span>{imageUploadError}</span>
+          </div>
+        )}
+
         {signupFields.map((field, index) => {
           const { placeholder, name, type } = field;
 
@@ -145,7 +183,6 @@ export const Profile = () => {
             </label>
           );
         })}
-
         <button
           type="submit"
           className="flex items-center justify-center w-full mt-4 btn btn-secondary gap-x-2"
@@ -154,7 +191,6 @@ export const Profile = () => {
           {loading && <Loader2Icon className="w-5 h-5 animate-spin" />}
           Submit
         </button>
-
         <button
           className="flex items-center justify-center w-full btn btn-neutral gap-x-2"
           // @ts-ignore
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,7 @@ export default {
     },
   },
   daisyui: {
-    themes: ["retro"],
+    themes: ["lofi"],
   },
   plugins: [require("daisyui")],
 };
